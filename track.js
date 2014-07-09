398bffb971790c852502f91c44d7cd2dd911129a
Jimmy31071987 <Jimmy31071987@gmail.com>
2014-07-09 17:49:02 -0400

    Create track.js
    We need jquery.min.js to run this script.

---
+++ track.js
@@ -0,0 +1,22 @@
+var TRACK = TRACK || {};
+var AJAX = AJAX || {};
+
+/*arrayKeyvalue : associateArray */
+TRACK = function (arrayKeyvalue,url) {
+    this.requestPage=url;
+    this.arrayKeyvalue = arrayKeyvalue;
+    var ajaxrequest = new AJAX();
+    this.enter = function () {
+          var formdata = new FormData();
+          for (var v in this.arrayKeyvalue) {
+              formdata.append(v, this.arrayKeyvalue[v]);
+          }
+            ajaxrequest.request(this.requestPage, formdata).done(function (e) { return e; });
+        }
+    this.exit = function () { return;}
+  }
+AJAX = function () {
+      this.request = function (url, data) {
+          return $.ajax({ url: url, type: 'post', data: data, async: true, processData: false, contentType: false });
+       }
+}
